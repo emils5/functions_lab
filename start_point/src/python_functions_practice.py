a0f8732e453b644d71fe3899fbc39edf5e1aced4
Emil <emil.shiva@gmail.com>
2021-06-16 16:27:53 +0100

    test completed so far

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,43 @@
+def return_10():
+    return(10)
+
+def add(num1, num2):
+    return num1 + num2
+    add_result = add( 1, 2 )
+
+def subtract(num1, num2):
+    return num1 - num2
+    subtract_result = subtract( 10, 5 )
+
+def multiply(num1, num2):
+    return num1 * num2
+    multiply_result = multiply( 4, 2 )
+
+def divide(num1, num2):
+    return num1 / num2
+    divide_result = divide( 10, 2 )
+
+def length_of_string(string):
+    return len(string)
+    string = "A string of length 21"
+    string_length = length_of_string( string )
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+    
+# def add_string_as_number(num_1, num_2):
+#     return num_1 + num_2
+#     add_result = add_string_as_number( "1", "2" )
+
+def number_to_full_month_name(month_1):
+    month_1 = "January"
+    return month_1
+
+def number_to_full_month_name3(month_3):
+    month_3 = "March"
+    return month_3
+
+# def number_to_full_month_name(month_9):
+#     month_9 = "September"
+#     return month_9
+
